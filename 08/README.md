e4b244df0860bc57815e9522f0d7e646990898e7
Luca Cattaneo <luca.cattaneo@mekit.it>
2022-09-25 08:48:39 +0200

    08 Start

---
+++ 08/README.md
@@ -0,0 +1,5 @@
+#Sketch 08
+
+Just vanilla javascript and p5.js
+
+`#creativecoding #processing #p5js #generative #creativecodeart`
---
+++ 08/sketch.js
@@ -0,0 +1,87 @@
+let items = 8;
+let nLoop = 1;
+let x = 0;
+let y = 0;
+let itemSize = false;
+let fps = 60;
+
+let colors = [
+  '#000000',
+  '#fca311',
+  '#e5e5e5',
+  '#ffffff',
+];
+
+let itemList = [];
+
+recordSketch(false);
+
+function setup() {
+  createCanvas(1080, 1080);
+  responsiveSketch();
+  frameRate(fps);
+  recordSketchSetFps(fps);
+  background(255);
+  itemSize = width / items;
+
+  setupItemList();
+}
+
+function setupItemList(){
+  for (let i = 0; i < items; i++) {
+    for (let j = 0; j < items; j++) { 
+      itemList.push({i, j});
+    }
+  }
+}
+
+function draw() {
+  recordSketchPre();
+  rectMode(CENTER);
+  strokeCap(ROUND);
+
+  itemList.forEach(item => {
+    push();
+      translate(itemSize * item.i, itemSize * item.j);
+      translate(itemSize * 0.5, itemSize * 0.5);
+      // drawItem();
+    pop();  
+  });
+
+  recordSketchPost(15);
+}
+
+function drawItem(){
+  noStroke();
+  let shuffledColors = shuffle(colors);
+
+  fill(shuffledColors[0]);
+  rect(0, 0, itemSize, itemSize);
+
+  fill(shuffledColors[1]);
+
+  let rX = random(0, 1);
+  if (rX >= 0.5){ rX = -1; } else { rX = 1; }
+
+  let rY= random(0, 1);
+  if (rY>= 0.5){ rY= -1; } else { rY= 1; }
+
+  push();
+    translate(rX * itemSize * 0.5, rY * itemSize * 0.5);
+
+    if (rX == -1){
+      if (rY == 1){
+        rotate(-PI * 0.5);
+      }
+    } else {
+      if (rY == 1){
+        rotate(PI);
+      } else {
+        rotate(PI * 0.5);
+      }
+    }   
+    arc(0, 0, itemSize * 2, itemSize * 2, 0, PI * 0.5); 
+  pop();
+}
+
+
