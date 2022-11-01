fd0ac37ff04155cddb2cf442c8d1c3d499559353
Luca Cattaneo <luca.cattaneo@mekit.it>
2022-11-01 11:31:10 +0100

    Material and light studying

--- 10/sketch.js
+++ 10/sketch.js
@@ -1,12 +1,28 @@
-let items = 20;
+let items = 10;
 let fps = 30;
 let speed = 0.25;
+
+let colors = [
+  '#000000',
+  '#264653',
+  '#2a9d8f',
+  '#e9c46a',
+  '#f4a261',
+  '#e76f51',
+  '#fefae0',
+  '#ffffff'
+];
 
 function setup() {
   createCanvas(1080, 1080, WEBGL);
   responsiveSketch();
   frameRate(fps);
   recordSketchSetFps(fps);
+
+  let camDist = width;
+  camera(-camDist * 0.5, -camDist * 0.5, camDist, 0, 0, 0);
+  smooth();
+  // debugMode();
 
   // ortho(-width / 2, width / 2, height / 2, -height / 2, -100, 10000);
   // ortho();
@@ -20,7 +36,69 @@ function draw() {
   mPos = recordSketchMouseGet(mPos);
 
   orbitControl();
+  noStroke();
   // normalMaterial();
+  
+  // background(0);
+  // ambientLight(100);
+  lightSetup(mPos);
+  
+
+  // Draw mouse position
+  // if (mPos.x > 0 && mPos.y > 0){
+  //   translate(mPos.x, mPos.y);
+  //   box(60);
+  // }
+  // growingBoxes(mPos);
+
+  recordSketchPost(8);
+}
+
+function lightSetup(mPos){
+  background(0);
+  let itemSize = width / items;
+  
+  ambientLight(50);
+  let lightDist = itemSize * 5;
+  pointLight(250, 100, 100, lightDist, -lightDist, lightDist);
+  pointLight(250, 100, 100, -lightDist, -lightDist, -lightDist);
+  
+  pointLight(100, 100, 250, lightDist, -lightDist, -lightDist);
+  pointLight(100, 100, 250, -lightDist, -lightDist, lightDist);
+
+  // Gray Plane
+  specularMaterial(color(colors[4]));
+  shininess(100);
+  push();
+    translate(0, itemSize * 0.1, 0);  
+    box(width, itemSize * 0.01, height);
+  pop();
+
+  // Yellow box
+  // Traslate Up
+  push();
+    translate(0, -itemSize * 0.1, 0);
+    specularMaterial(color(colors[2]));
+    shininess(10);
+    box(itemSize, itemSize * 0.2, itemSize);
+  pop();
+
+  push();
+    translate(itemSize, -itemSize * 0.5, 0);
+    specularMaterial(color(colors[3]));
+    shininess(10);
+    box(itemSize, itemSize, itemSize);
+  pop();
+
+  push();
+    translate(0, -itemSize * 0.7, 0);
+    specularMaterial(color(colors[4]));
+    shininess(10);
+    sphere(itemSize * 0.5);
+  pop();
+}
+
+function growingBoxes(mPos){
   ambientLight(150);
 
   directionalLight(color('red'), -1, -1, -1);
@@ -63,15 +141,6 @@ function draw() {
       pop();        
     }
   }
-
-
-  // Draw mouse position
-  // if (mPos.x > 0 && mPos.y > 0){
-  //   translate(mPos.x, mPos.y);
-  //   box(60);
-  // }
-
-  recordSketchPost(8);
 }
 
 
