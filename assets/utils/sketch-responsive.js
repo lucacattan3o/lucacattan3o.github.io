6da48c5939cd857ed54040ce1038fbce6eabf90a
Luca Cattaneo <luca.cattaneo@mekit.it>
2023-04-14 18:51:13 +0200

    Merged

--- assets/utils/sketch-responsive.js
+++ assets/utils/sketch-responsive.js
@@ -1,27 +1,80 @@
+/**
+* @file A simple plugin library to make your p5.js sketch responsive
+* It works also in p5.js editor
+* @author Luca Cattaneo <luca.cattaneo@mekit.it>
+* @copyright Luca Cattaneo 2023
+* @license MIT License
+*/
+
+// todo: define a global object to store all the variables
+
 let responsiveMaxWidth, responsiveMaxHeight;
-let domCanvas;
+let responsiveDomCanvas;
 let responsiveScaleFactor;
+let responsiveCanvasMargin;
 
-const responsiveCanvasMargin = 80;
+/**
+ * Make your sketch resposive
+ * @summary Use this function inside p5.js setup() function 
+ * @param {Object} options - custom options
+ * @param {HTMLElement} options.el - dom element to scale
+ * @param {Number} options.margin - margin left aroud the sketch
+ */
+function responsiveSketch(options){
 
-function responsiveSketch(){
-  pixelDensity(1);
-  setVars();
+  let defaultSettings = {
+    el: document.getElementsByTagName('main')[0],
+    margin: 80,
+  };
+
+  if (!options) options = {};
+
+  // Extend options
+  var settings = {};
+  for(var key in defaultSettings){
+    if(options.hasOwnProperty(key)){
+      settings[key] = options[key];
+    } else {
+      settings[key] = defaultSettings[key];
+    }
+  }
+
+  responsiveCanvasMargin = settings.margin;
+  responsiveDomCanvas = settings.el;
+
+  responsiveMaxWidth = width;
+  responsiveMaxHeight = height;
+
   responsiveCanvas(); 
 }
 
-function setVars(){
-  domCanvas = document.getElementsByTagName('main')[0];
-  responsiveMaxWidth = width;
-  responsiveMaxHeight = height;
+/**
+ * Get the correct position of the mouse on the the sketch
+ * - define a global variable at the beginning of your sketch: let mPos;
+ * - use this function inside p5.js draw() function
+ * - mPos = responsiveMousePos();
+ * - instead of mouseX you can now use mPos.x
+ * - instead of mouseY you can now use mPos.y
+ * @return {Object} an object representing the position of the mouse
+ */
+function responsiveMousePos(){
+  return {
+    x: mouseX * (1 / responsiveScaleFactor),
+    y: mouseY * (1 / responsiveScaleFactor)
+  };
 }
 
+/**
+ * Update the scale when the window is resized
+ */
 function windowResized() {
   responsiveCanvas();
 }
 
+/**
+ * Scale the sketch
+ */
 function responsiveCanvas(){
-  // can also be used in sketch
   responsiveScaleFactor = 1;
 
   let availableWidth = window.innerWidth - responsiveCanvasMargin;
@@ -35,12 +88,6 @@ function responsiveCanvas(){
     responsiveScaleFactor = minScaleFactor;
   }
   
-  domCanvas.style = "transform: scale(" + responsiveScaleFactor + ")";
+  responsiveDomCanvas.style = "transform: scale(" + responsiveScaleFactor + ")";
 }
 
-function responsiveMousePos(){
-  return {
-    x: mouseX * (1 / responsiveScaleFactor),
-    y: mouseY * (1 / responsiveScaleFactor)
-  };
-}
