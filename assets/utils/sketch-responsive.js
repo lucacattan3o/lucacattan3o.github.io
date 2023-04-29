fd959299e7dd2e08be7369c9019ad84f6f210855
Luca Cattaneo <luca.cattaneo@mekit.it>
2023-04-29 08:34:57 +0200

    Sketch Responsive updated

--- assets/utils/sketch-responsive.js
+++ assets/utils/sketch-responsive.js
@@ -1,34 +1,39 @@
 /**
-* @file A simple plugin library to make your p5.js sketch responsive
-* - It works also in p5.js editor
+* https://github.com/lucacattan3o/p5.js-responsive-sketch
+* A simple library to make your p5.js sketch responsive
+* It works also in p5.js editor
 * @author Luca Cattaneo <luca.cattaneo@mekit.it>
-* {@link https://lucacattan3o.github.io/}
-* @version 1.0.0
-* @copyright Luca Cattaneo 2023
-* @license MIT License
+* @version 1.0.1
+* @license MIT
 */
 
+"use strict";
+
 let rSketch = {
+  domEl: null,
+  domParent: null,
+  margin: 0,
   maxWidth: null,
   maxHeight: null,
-  domEl: null,
   scaleFactor: 1,
-  margin: 0,
 };
 
 /**
  * Make your sketch resposive
  * @summary Use this function inside p5.js setup() function 
  * @param {Object} options - custom options
+ * @param {Bolean} options.centerOnPage - whether the sketch should be centered on the page
  * @param {HTMLElement} options.el - dom element to scale
- * @param {Number} options.margin - margin left aroud the sketch
+ * @param {Number} options.margin - margin around the sketch
+ * @param {Number} options.pixelDensity - set a custom pixel density
  */
 function responsiveSketch(options){
 
   // Default settings
   let defaultSettings = {
-    el: document.getElementsByTagName('main')[0],
+    el: document.getElementsByTagName('canvas')[0],
     margin: 80,
+    centerOnPage: true,
     pixelDensity: 1,
   };
 
@@ -44,13 +49,31 @@ function responsiveSketch(options){
     }
   }
 
-  rSketch.margin = settings.margin;
+  rSketch.centerOnPage = settings.centerOnPage;
   rSketch.domEl = settings.el;
+  rSketch.domParent = settings.el.parentNode;
+  rSketch.margin = settings.margin;
   rSketch.maxWidth = width;
   rSketch.maxHeight = height;
 
   if (settings.pixelDensity !== null){
     pixelDensity(settings.pixelDensity);
+  }
+
+  if (rSketch.centerOnPage){
+    let domBody = document.getElementsByTagName('body')[0];
+    let domHtml = document.getElementsByTagName('html')[0];
+    
+    let pageStyle = `
+      display: flex;
+      justify-content: center;
+      align-items: center;  
+      width: 100%;
+      height: 100%;
+    `;
+
+    domBody.style = pageStyle;
+    domHtml.style = pageStyle;
   }
   
   responsiveCanvas(); 
@@ -93,7 +116,22 @@ function responsiveCanvas(){
   if (minScaleFactor < 1){
     rSketch.scaleFactor = minScaleFactor;
   }
-  
-  rSketch.domEl.style = "transform: scale(" + rSketch.scaleFactor + ")";
+
+  let w = width * rSketch.scaleFactor;
+  let h = height * rSketch.scaleFactor;
+
+  rSketch.domEl.style = `transform: scale(${rSketch.scaleFactor});`;
+
+  rSketch.domParent.style = `
+    display: flex;
+    justify-content: center;
+    align-items: center;  
+    width: ${w}px;
+    height: ${h}px;
+  `;
 }
 
+if (typeof exports !== "undefined"){
+  exports.responsiveSketch = responsiveSketch;
+  exports.responsiveMousePos = responsiveMousePos;
+}
