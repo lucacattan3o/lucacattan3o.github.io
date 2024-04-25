b0d744aa242bb0c7e52f875a1f5c4615a9cdfaf7
Luca Cattaneo <luca.cattaneo@mekit.it>
2024-04-25 10:05:24 +0200

    Density

--- w-noise/sketch.js
+++ w-noise/sketch.js
@@ -1,8 +1,7 @@
 let fps = 60;
 
 let obj = {
-  itemsX: 9 * 2,
-  itemsY: 16 * 2,
+  density: 2,
 };
 
 let storageName = 'gui-noise';
@@ -33,18 +32,24 @@ function setup() {
 
 function draw() {
   background(0);
+
+  let itemsX = 9 * obj.density;
+  let itemsY = 16 * obj.density;
   
-  itemSizeW = width  / obj.itemsX;
-  itemSizeH = height / obj.itemsY;
+  itemSizeW = width  / itemsX;
+  itemSizeH = height / itemsY;
 
   mPos = responsiveMousePos();
 
   let offsetX = -mPos.x * 0.001;
   offsetX = frameCount * 0.005;
+  // offsetX = 0;
   let offsetY = -mPos.y * 0.001;
+  offsetY = frameCount * 0.005;
+  // offsetY = 0;
 
-  for (i = 0; i < obj.itemsX + 20; i++){
-    for (j = 0; j < obj.itemsY; j++){
+  for (i = 0; i < itemsX + 20; i++){
+    for (j = 0; j < itemsY; j++){
       let x = i * itemSizeW;
       let y = j * itemSizeH;
       
@@ -54,7 +59,12 @@ function draw() {
         // -- Item start -- 
         let noiseScale = 0.1;
         // let z = map(getLoopBounce(0.125), -1, 1, 0, 1); 
-        let n = noise(i * noiseScale + offsetX, j * noiseScale + offsetY);   
+        let n = noise(i * noiseScale + offsetX, j * noiseScale + offsetY); 
+        
+        let nb = noise(
+          (i + 100) * noiseScale + offsetX,
+          (j + 100) * noiseScale + offsetY
+        );
         
         // Construction grid
         push();
@@ -67,7 +77,7 @@ function draw() {
         push();
           if (n > 0.3 && n <= 0.4){
             // arrow
-            stroke(palette[1]);
+            stroke(palette[5]);
             strokeWeight(itemSizeW * 0.1);
             line(itemSizeW * 0.1, itemSizeH * 0.1, itemSizeW * 0.8, itemSizeH * 0.1);
             line(itemSizeW * 0.1, itemSizeH * 0.1, itemSizeW * 0.1, itemSizeH * 0.8);
@@ -75,26 +85,65 @@ function draw() {
           }
           if (n > 0.4 && n <= 0.5){
             // triangle
-            fill(palette[2]);
-            stroke(0);
+            fill(palette[0]);
+            noStroke();
             triangle(
-              itemSizeW, 0,
-              itemSizeW, itemSizeH,
-              0, itemSizeH
+              itemSizeW * 0.9, itemSizeH * 0.1,
+              itemSizeW * 0.9, itemSizeH * 0.9,
+              itemSizeW * 0.1, itemSizeH * 0.9
             );
           }
           if (n > 0.5 && n < 0.7){
             // square
-            fill(palette[3]);
-            stroke(0);
+            fill(palette[2]);
+            noStroke();
             rect(itemSizeW * 0.1, itemSizeH * 0.1, itemSizeW * 0.8, itemSizeH * 0.8);
           }
+
+
+          if (n > 0.1 && n < 0.2){
+            push();
+            // dots
+            translate(itemSizeW * 0.5, itemSizeH * 0.5);
+            fill(palette[4]);
+            noStroke();
+            // circle(0, 0, itemSizeW * 0.5);
+            pop();
+          }
+
+          if (n > 0.1 && n < 0.25){
+            push();
+              // plus
+              stroke(palette[4]);
+              strokeWeight(itemSizeW * 0.1);
+              line(itemSizeW * 0.1, itemSizeH * 0.5, itemSizeW * 0.9, itemSizeH * 0.5);
+              line(itemSizeW * 0.5, itemSizeH * 0.1, itemSizeW * 0.5, itemSizeH * 0.9);
+            pop();
+          }
+
+          /*
+          if (nb > 0.7 && nb < 0.8){
+            stroke(palette[2]);
+            strokeWeight(itemSizeW * 0.1);
+            line(itemSizeW * 0.1, itemSizeH * 0.1, itemSizeW * 0.9, itemSizeH * 0.9);
+            line(itemSizeW * 0.1, itemSizeH * 0.9, itemSizeW * 0.9, itemSizeH * 0.1);
+          }
+          */
+          
       
         pop();
       
         // -- Item end -- 
       pop();
     }
+  }
+
+  if (frameCount == 1){
+    sketchExportStart();
+  }
+  sketchExport();
+  if (frameCount == 15 * fps){
+    sketchExportEnd();
   }
 }  
 
@@ -145,8 +194,7 @@ function setupLil(){
   gui = new GUI();
 
   const grid = gui.addFolder('Grid');
-  grid.add(obj, 'itemsX').min(1).max(40).step(1).name('Items X');
-  grid.add(obj, 'itemsY').min(1).max(40).step(1).name('Items Y');
+  grid.add(obj, 'density').min(1).max(5).step(1).name('Density');
 
   gui.add(obj, 'savePreset' ).name('Save Preset');
   gui.add(obj, 'clearStorage').name('Clear Preset');
