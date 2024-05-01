bae63b6b676f32d4462dffd7a02311acaebae0c3
Luca Cattaneo <luca.cattaneo@mekit.it>
2024-05-01 12:18:36 +0200

    Clean Up

--- w-camera/sketch.js
+++ w-camera/sketch.js
@@ -2,20 +2,15 @@ let fps = 5;
 
 let items = 60;
 
-let margin = 0.1;
-let sketchWidth;
-let sketchMargin;
 let itemSize;
 
 let noiseDetail = 0.1;
 let zOffset = 0;
 
-let chars = " ·.:,;#'+*`=?!¬”#^˜·$%/()";
-chars = '  ·”#^˜·$%/()';
-
 let capture;
-let backgroundPixels = null;
-let bgLightness = []; 
+
+// let backgroundPixels = null;
+// let bgLightness = []; 
 
 function setup() {
   createCanvas(1080, 1080);
@@ -30,21 +25,12 @@ function setup() {
 }
 
 function draw() {
-  sketchMargin = width * margin;
-  sketchWidth = width - sketchMargin;
-
-  itemSize = sketchWidth / items;
-
+  itemSize = width / items;
   capture.loadPixels();
 
   background(0);
   rectMode(CENTER);
   noStroke();
-  translate(sketchMargin * 0.5, sketchMargin * 0.5);
-
-  textFont('monospace');
-  textSize(itemSize * 1.1);
-  textAlign(CENTER, CENTER);
 
   let ci = 0;
   for (let i = 0; i < items; i++) {
@@ -54,22 +40,15 @@ function draw() {
       let y = j * itemSize;
 
       let c = getVideoColorAtPosition(i, j);
-
-      // let n = noise(i * noiseDetail, j * noiseDetail, zOffset);
-      // let b = brightness(c);
-      // let index = floor(map(b, 0, 200, 0, chars.length, true));
       
       push();
         translate(x, y);
         translate(itemSize * 0.5, itemSize * 0.5);
+        
         // Grid
         noFill();
-        // stroke(200);
-        // rect(0, 0, itemSize),
-        
-        // fill(b);
-        // rect(0, 0, itemSize * 1);
-        
+        stroke(200);
+        // rect(0, 0, itemSize);
 
         let l = lightness(c);
         let light = map(l, 0, 100, 0, 1);
@@ -101,7 +80,6 @@ function draw() {
         //   line(0, 0, itemSize, itemSize);
         // }
 
-        
         // if (index == 7 || index == 8){
         //   noFill();
         // }
@@ -110,12 +88,8 @@ function draw() {
         // }
         // text(chars.charAt(index), 0, 0);
       pop();
-
-      // prevItems.push(c);
     }
   }
-
-  // zOffset += 0.005;
 
   if (frameCount == 1){
     sketchExportStart();
@@ -126,6 +100,52 @@ function draw() {
   }
 }
 
+function getVideoColorAtPosition(i, j){
+  let videoSize = capture.height / items;
+  let videoMargin = floor((capture.width - capture.height) * 0.5);
+
+  let x = floor(capture.width - (i * videoSize) - videoMargin);
+  let y = floor(j * videoSize);
+
+  // Get index of the pixel (based by 4)
+  let pIndex = (y * capture.width + x) * 4;
+
+  // Rgba color
+  let r = capture.pixels[pIndex + 0];
+  let g = capture.pixels[pIndex + 1];
+  let b = capture.pixels[pIndex + 2];
+  let a = capture.pixels[pIndex + 3];
+
+  return color(r, g, b, a);
+}
+
+function colorDistance(first, second) {
+  let r = abs(red(first) - red(second));
+  let g = abs(green(first) - green(second));
+  let b = abs(blue(first) - blue(second));
+  return r + g + b;
+}
+
+/*
+if (backgroundPixels){
+  // Rgba color
+  let r = backgroundPixels[pIndex + 0];
+  let g = backgroundPixels[pIndex + 1];
+  let b = backgroundPixels[pIndex + 2];
+  let a = backgroundPixels[pIndex + 3];  
+
+  let back = color(r, g, b, a);
+
+  let d = colorDistance(c, back);
+  if (d > 100){
+    return c;
+  }
+}
+return null;
+*/
+
+
+/*
 function mouseClicked() {
   // invece che storare il colore di ogni pixel
   // dovrei storare la luminosità dell'item
@@ -150,50 +170,5 @@ function setBgLightness(){
       bgLightness.push(ligth);
     }
   }
-  console.debug(bgLightness);
 }
-
-function getVideoColorAtPosition(i, j){
-
-  let videoSize = capture.height / items;
-  let videoMargin = floor((capture.width - capture.height) * 0.5);
-
-  let x = floor(capture.width - (i * videoSize) - videoMargin);
-  let y = floor(j * videoSize);
-
-  // Get index of the pixel (based by 4)
-  let pIndex = (y * capture.width + x) * 4;
-
-  // Rgba color
-  let r = capture.pixels[pIndex + 0];
-  let g = capture.pixels[pIndex + 1];
-  let b = capture.pixels[pIndex + 2];
-  let a = capture.pixels[pIndex + 3];
-
-  let c = color(r, g, b, a);
-  return c;
-
-  if (backgroundPixels){
-    // Rgba color
-    let r = backgroundPixels[pIndex + 0];
-    let g = backgroundPixels[pIndex + 1];
-    let b = backgroundPixels[pIndex + 2];
-    let a = backgroundPixels[pIndex + 3];  
-
-    let back = color(r, g, b, a);
-
-    let d = colorDistance(c, back);
-    if (d > 100){
-      return c;
-    }
-  }
-
-  return null;
-}
-
-function colorDistance(first, second) {
-  let r = abs(red(first) - red(second));
-  let g = abs(green(first) - green(second));
-  let b = abs(blue(first) - blue(second));
-  return r + g + b;
-}
+*/
