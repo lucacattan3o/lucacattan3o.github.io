3f8857dfdbaccc38a8b369de6ebbf91e39545ae5
Luca Cattaneo <luca.cattaneo@mekit.it>
2024-05-01 12:27:16 +0200

    Lil Start

--- w-camera/sketch.js
+++ w-camera/sketch.js
@@ -1,8 +1,13 @@
 let fps = 5;
-let items = 60;
 
 let itemSize;
 let capture;
+
+let storageName = 'gui-camera';
+
+let obj = {
+  items: 10,
+};
 
 // let backgroundPixels = null;
 // let bgLightness = []; 
@@ -15,13 +20,14 @@ function setup() {
     fps: fps,
     name: getFileName('video'),
   });
+  setupLil();
 
   capture = createCapture(VIDEO);
   capture.hide();
 }
 
 function draw() {
-  itemSize = width / items;
+  itemSize = width / obj.items;
   capture.loadPixels();
 
   background(0);
@@ -29,8 +35,8 @@ function draw() {
   noStroke();
 
   let ci = 0;
-  for (let i = 0; i < items; i++) {
-    for (let j = 0; j < items; j++) {
+  for (let i = 0; i < obj.items; i++) {
+    for (let j = 0; j < obj.items; j++) {
       ci++;
       let x = i * itemSize;
       let y = j * itemSize;
@@ -97,7 +103,7 @@ function draw() {
 }
 
 function getVideoColorAtPosition(i, j){
-  let videoSize = capture.height / items;
+  let videoSize = capture.height / obj.items;
   let videoMargin = floor((capture.width - capture.height) * 0.5);
 
   let x = floor(capture.width - (i * videoSize) - videoMargin);
@@ -126,6 +132,86 @@ function getFileName(prefix){
   let now = new Date();
   return prefix + '-' + now.getMonth() + '-' + now.getDay() + '-' + now.getHours() + '-' + now.getMinutes() + '-' + now.getSeconds();
 }
+
+// ** LIL **
+// ---------
+
+let GUI = lil.GUI;
+let gui;
+
+obj.savePreset = function() {
+  saveToStorage();
+};
+
+obj.loadPreset = function() {
+  gui.load(preset);
+};
+
+obj.export = function() {
+  saveToStorage();
+  let url = window.location.href;    
+  if (url.indexOf('?') > -1){
+    url += '&export=true';
+  } else {
+    url += '?export=true';
+  }
+  window.location.href = url;
+};
+
+obj.clearStorage = function() {
+  localStorage.removeItem(storageName);
+  window.location = window.location.href.split("?")[0];
+};
+
+obj.startOver = function(){
+  saveToStorage();
+  window.location = window.location.href.split("?")[0];
+};
+
+obj.stopExport = function(){
+  sketchExportEnd();
+};
+
+obj.saveImage = function(){
+  saveCanvas(getFileName('visual'), 'png');
+}
+
+function setupLil(){
+  gui = new GUI();
+
+  const grid = gui.addFolder('Grid');
+  grid.add(obj, 'items').min(5).max(60).step(1).name('Items');
+
+  gui.add(obj, 'savePreset' ).name('Save Preset');
+  gui.add(obj, 'clearStorage').name('Clear');
+  gui.add(obj, 'startOver').name('Run Again');
+
+  let exportBtn = gui.add(obj, 'export').name('Export Video');
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  if (urlParams.get('export') == 'true'){
+    console.debug('test');
+    exportBtn.disable();
+    exportBtn.name('Exporting...');
+
+    gui.add(obj, 'stopExport').name('Stop Export');
+  }
+  
+  gui.add(obj, 'saveImage').name('Save Image');
+
+  // gui.onChange( event => {});
+  
+  let saved = localStorage.getItem(storageName);
+  if (saved){
+    gui.load(JSON.parse(saved));
+  };
+};
+
+function saveToStorage(){
+  preset = gui.save();
+  localStorage.setItem(storageName, JSON.stringify(preset));
+};
+
 
 /*
 if (backgroundPixels){
