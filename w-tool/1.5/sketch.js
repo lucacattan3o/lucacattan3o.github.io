de70e694c282578d081ffc231e6e955c916adc0a
Luca Cattaneo <luca.cattaneo@mekit.it>
2025-01-24 15:59:29 +0100

    Level test

---
+++ w-tool/1.5/sketch.js
@@ -0,0 +1,151 @@
+let fps = 30;
+
+let palette = [
+  "ffffff",
+  "ffbe0b",
+  "3a86ff",
+  "5900b3",
+  "000000",
+];
+
+let stereoColors = null;
+let w, h;
+
+let bg = '#000000';
+let mPos;
+let font;
+let dmImage = null;
+let redrawImage = false;
+
+let x = 0;
+
+function preload(){
+  font = loadFont('./../common/fonts/KolkerBrush-Regular.ttf');
+}
+
+function setup() {
+  setupCanvas();
+  setupLil();
+  background(bg);
+  rectMode(CENTER);
+}
+
+function setupCanvas(){
+  w = floor(obj.canvasW * obj.canvasMulty);
+  h = floor(obj.canvasH * obj.canvasMulty);
+  createCanvas(w, h);
+  responsiveSketch();
+  frameRate(fps);
+
+  let img = document.getElementById('stereogram');
+  img.width = w;
+  img.height = h;
+}
+
+function draw() {
+  drawLevels();
+  // drawImage()
+  // drawPaint();
+}
+
+function drawLevels(){
+  let items = 500;
+  let itemSize = height / items;
+  let yScale = 0.003;
+  let xScale = 0.001;
+  push();
+    noStroke();
+    translate(width * 0.5, 0);
+    for (let i = 0; i < items; i++) {
+      let y = i * itemSize;
+      push();
+        translate(0, y);
+        let c = map(noise(x * xScale, y * yScale), 0.3, 0.5, 0, 255);
+        fill(c)
+        circle(x, 0, itemSize * 1.2);
+        let c2 = map(noise(-x * xScale, y * yScale), 0.3, 0.5, 0, 255);
+        fill(c2)
+        circle(-x, 0, itemSize * 1.2);
+      pop();
+    }
+  pop();
+  x = x + itemSize * 0.2;
+}
+
+function drawRivers() {
+  for (i = 0; i < items.length; i++){
+    let item = items[i];
+    item.update();
+    item.draw()
+  }
+
+  let radius = width * 0.5 * obj.rivRadius;
+  stroke(255);
+  noFill();
+  circle(width * 0.5, height * 0.5, radius * 2);
+  
+  let sec = frameCount / fps;
+  if (sec % 1 == 0){
+    // console.debug(sec);
+  }
+}
+
+function drawImage(){
+  if (dmImage){
+    if (redrawImage){
+      background(0);
+      push();
+      translate(w * 0.5, h * 0.5);
+      translate(w * obj.dmX, h * obj.dmY);
+      scale(obj.dmScale);
+      image(dmImage, -dmImage.width * 0.5, -dmImage.height * 0.5);
+      pop();
+      redrawImage = false;
+    }
+  }
+}
+
+function drawPaint(){
+  mPos = responsiveMousePos();
+  if (mouseIsPressed && keyIsPressed && keyCode == 32){
+    let bSize = 100;
+    let fillSize = bSize * obj.brushSize;
+    let strokeSize = 1;
+    // console.debug('Original: ' + fillSize);
+    // da 0 alla dimensione del brush
+    let hard = (1 - obj.brushHard) * fillSize;
+    // console.debug('Hard: ' + hard);
+    // riduco la dimensione del fill
+    fillSize = fillSize - hard;
+    // console.debug('Size: ' + fillSize);
+    
+    push();
+    translate(mPos.x, mPos.y);
+    let rgb = [
+      obj.brushColor[0] * 255,
+      obj.brushColor[1] * 255,
+      obj.brushColor[2] * 255
+    ];
+    fill(rgb);
+    noStroke();
+    circle(0, 0, fillSize);
+    
+    if (hard){
+      for (i = 0; i < hard; i++){
+        let sSize = fillSize + (i * strokeSize);
+        let alpha = map(i, 0, hard, 255, 0, true);
+        let rgba = [
+          obj.brushColor[0] * 255,
+          obj.brushColor[1] * 255,
+          obj.brushColor[2] * 255,
+          alpha,
+        ];
+        strokeWeight(strokeSize);
+        stroke(rgba);
+        noFill();
+        circle(0, 0, sSize);
+      }
+    }
+    pop();
+  }
+}
